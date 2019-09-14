0afd3e4e9158fbcad3ed10b0f34a7e9af9de8285
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-09-14 02:37:56 -0500

    added db-config

---
+++ migrations/db-config.js
@@ -0,0 +1,5 @@
+const knex = require('knex');
+
+const knexConfig = require('../knexfile.js');
+const enviroment = process.env.DB_ENV || 'development'
+module.exports = knex(knexConfig[enviroment]);
