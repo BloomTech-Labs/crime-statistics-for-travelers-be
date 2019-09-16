a6d0e8c462d989fb3b98c9c503fff98c0f5d8188
Jengopockets <jengopockets@gmail.com>
2019-09-16 18:20:49 -0400

    Fixed Migrations, Added Forein Keys to table

--- migrations/20190914020937_user_locations.js
+++ migrations/20190914020937_user_locations.js
@@ -1,10 +1,24 @@
 
 exports.up = function(knex) {
-    return knex.schema.createTable('user_locations', (users) => {
+    return knex.schema.createTable('user_locations', user_locations => {
         user_locations.increments();
-        // user_locations.integer("user_id");
-        // user_locations.integer('location_id');
-        // user_locations.text('comment');
+        user_locations
+            .integer('user_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE');
+        user_locations
+            .integer('location_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('locations')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE');
+        user_locations.string('comment', 400);
         
  
     })
