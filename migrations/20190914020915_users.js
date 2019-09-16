1c92efbf240928b5c8a467f024333610079dd69b
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-09-16 11:07:25 -0500

    remove unique

--- migrations/20190914020915_users.js
+++ migrations/20190914020915_users.js
@@ -4,7 +4,7 @@ exports.up = function(knex) {
       users.increments();
       users.string('username',128).unique();
       users.string('email',128).unique();
-      users.string('password',128).unique();
+      users.string('password',128)
       users.string('gender',15);
       users.integer('age');
       users.text('race',25);
